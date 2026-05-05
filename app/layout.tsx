d53af78f05140cd9551a9f57607f7bd05b5bf651
Hazim2024 <hazimiftikhar@gmail.com>
2026-05-05 13:01:31 +0100

    Added icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { TransactionProvider } from "@/context/TransactionContext";
 import AppShell from "../components/AppShell";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const mono = JetBrains_Mono({ subsets: ["latin"], variable: "--font-mono" });
@@ -20,12 +21,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#3B82F6" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </head>
       <body className={`${inter.variable} ${mono.variable} font-sans bg-[#050505] text-white min-h-screen`}>
         <AuthProvider>
           <TransactionProvider>
             <AppShell>{children}</AppShell>
           </TransactionProvider>
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
